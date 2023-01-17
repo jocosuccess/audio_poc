a0d672b8d21caca1973c7c2d319908b67ef9e0b0
jocosuccess <highfamous@outlook.com>
2023-01-17 13:19:42 +0100

    add jack client connection

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import jack
 print('*** List of Sound Devices ***')
 print(sd.query_devices())
 print('*****************************')
-#client = jack.Client('PocClient')
-#print('*** List of JACK Ports ***')
-#print(client.get_ports())
+client = jack.Client('PocClient', no_start_server=True, servername='poc')
+if client.status.server_failed:
+    print(f'{jack.Status.server_failed}--------JACK server was failed')
+else:
+    print('JACK server was already running')
+print('*** List of JACK Ports ***')
+print(client.get_ports())
