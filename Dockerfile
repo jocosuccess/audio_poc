e9cc94d69bc404762c38da23acf2cd49e96b2d90
jocosuccess <highfamous@outlook.com>
2023-01-18 14:29:46 +0100

    update for jack connection

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM python:3.7.10-buster
 
 # install app dependencies
-RUN apt-get update && apt-get install -y python3 python3-pip libportaudio2 libasound-dev libportaudiocpp0 portaudio19-dev
+
+RUN apt-get update && apt-get install -y libjack-jackd2-dev jackd2 alsa-tools python3 python3-pip libportaudio2 libasound-dev libportaudiocpp0 portaudio19-dev
 RUN apt-get install  
 
 RUN mkdir /project
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import sounddevice as sd
 import jack
 print('*** List of Sound Devices ***')
 print(sd.query_devices())
-print('*****************************')
-client = jack.Client('PocClient', no_start_server=True, servername='poc')
-if client.status.server_failed:
-    print(f'{jack.Status.server_failed}--------JACK server was failed')
-else:
-    print('JACK server was already running')
-print('*** List of JACK Ports ***')
-print(client.get_ports())
+# print('*****************************')
+# client = jack.Client('PocClient', no_start_server=True)
+# if client.status.server_failed:
+#     print(f'{jack.Status.server_failed}--------JACK server was failed')
+# else:
+#     print('JACK server was already running')
+# print('*** List of JACK Ports ***')
+# print(client.get_ports())
